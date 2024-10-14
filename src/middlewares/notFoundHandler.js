3ec3fecc62df85a8c5fc3108b5275557d0700c54
AllaHanol <156720109+AllaHanol@users.noreply.github.com>
2024-10-14 14:47:17 +0300

    hw3

--- src/middlewares/notFoundHandler.js
+++ src/middlewares/notFoundHandler.js
@@ -2,6 +2,6 @@ import createHttpError from 'http-errors';
 
 export const notFoundHandler = (req, res, next) => {
   res.status(404).json({
-    error: createHttpError(404, 'RouteNot Found'),
+    error: createHttpError(404, 'Route not Found'),
   });
 };
