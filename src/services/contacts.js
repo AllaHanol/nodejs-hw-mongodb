aa9d815f381d29a9c3c9a5b23c79e16281251f03
AllaHanol <156720109+AllaHanol@users.noreply.github.com>
2024-11-01 22:24:13 +0200

    userId added

--- src/services/contacts.js
+++ src/services/contacts.js
@@ -1,4 +1,4 @@
-import { ContactsCollection } from '../db/models/contacts.js';
+import { ContactsCollection } from '../db/modules/contacts.js';
 import { calculatePaginationData } from '../utils/calculatePaginationData.js';
 import { SORT_ORDER } from '../constants/constants.js';
 
