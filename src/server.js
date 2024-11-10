7eeca0d2b1174a53da4845f144aa209d3253629f
AllaHanol <156720109+AllaHanol@users.noreply.github.com>
2024-11-10 19:25:35 +0200

    swagger ended

--- src/server.js
+++ src/server.js
@@ -11,11 +11,15 @@ import router from './routers/index.js';
 import { errorHandler } from './middlewares/errorHandler.js';
 import { notFoundHandler } from './middlewares/notFoundHandler.js';
 import { UPLOAD_DIR } from './constants/constants.js';
+import { swaggerDocs } from './middlewares/swaggerDocs.js';
 
 const PORT = Number(env('PORT', '3000'));
 
 export const setupServer = () => {
   const app = express();
+
+  app.use('/uploads', express.static(UPLOAD_DIR));
+  app.use('/api-docs', swaggerDocs());
 
   app.use(
     express.json({
